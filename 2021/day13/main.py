547053aad12db77443603a82fe66fbb5acb557c7
James MacAdie <james@macadie.co.uk>
2021-12-13 18:30:29 +0000

    Day 13 #6
    Added type hints and moved the method call order around to make a
    bit more sense

--- 2021/day13/main.py
+++ 2021/day13/main.py
@@ -1,60 +1,18 @@
+from typing import List
+
+
 class Origami:
 
     MAX = 2000
 
-    def add_point(self, point):
-        x, y = [int(i) for i in point.split(',')]
-        self.map[y][x] = 1
-        self.max_x = max(self.max_x, x + 1)
-        self.max_y = max(self.max_y, y + 1)
+    def __init__(self, file: str) -> None:
+        self.map = [[0 for _ in range(self.MAX)] for _ in range(self.MAX)]
+        self.max_x = 0
+        self.max_y = 0
+        self.fold1_dots = 0
+        self.read_file(file)
 
-    def trim_map(self):
-        self.map = self.map[:self.max_y]
-        for y in range(self.max_y):
-            self.map[y] = self.map[y][:self.max_x]
-
-    def fold_x(self, fold_at):
-        lower = max(2 * fold_at - self.max_x + 1, 0)
-        for x in range(lower, fold_at):
-            for y in range(self.max_y):
-                self.map[y][x] = min(self.map[y][x] + self.map[y][2 * fold_at - x], 1)
-        self.max_x = fold_at
-
-    def fold_y(self, fold_at):
-        lower = max(2 * fold_at - self.max_y + 1, 0)
-        for x in range(self.max_x):
-            for y in range(lower, fold_at):
-                self.map[y][x] = min(self.map[y][x] + self.map[2 * fold_at - y][x], 1)
-        self.max_y = fold_at
-
-    def do_fold(self, fold):
-        dim, fold_at = fold.split('=')
-        fold_at = int(fold_at)
-        if dim == 'x':
-            self.fold_x(fold_at)
-        elif dim == 'y':
-            self.fold_y(fold_at)
-        self.trim_map()
-
-    def count_dots(self):
-        sum_dots = 0
-        for x in range(self.max_x):
-            for y in range(self.max_y):
-                sum_dots += self.map[y][x]
-        return sum_dots
-
-    def add_points(self, points):
-        for point in points:
-            self.add_point(point)
-        self.trim_map()
-
-    def do_folds(self, folds):
-        for fold in folds:
-            self.do_fold(fold)
-            if self.fold1_dots == 0:
-                self.fold1_dots = self.count_dots()
-
-    def read_file(self, file_path):
+    def read_file(self, file_path: str) -> None:
         points = []
         folds = []
         points_section = True
@@ -71,14 +29,59 @@ class Origami:
         self.add_points(points)
         self.do_folds(folds)
 
-    def __init__(self, file):
-        self.map = [[0 for _ in range(self.MAX)] for _ in range(self.MAX)]
-        self.max_x = 0
-        self.max_y = 0
-        self.fold1_dots = 0
-        self.read_file(file)
+    def add_points(self, points: List[str]) -> None:
+        for point in points:
+            self.add_point(point)
+        self.trim_map()
 
-    def print_out(self):
+    def add_point(self, point: str) -> None:
+        x, y = [int(i) for i in point.split(',')]
+        self.map[y][x] = 1
+        self.max_x = max(self.max_x, x + 1)
+        self.max_y = max(self.max_y, y + 1)
+
+    def trim_map(self) -> None:
+        self.map = self.map[:self.max_y]
+        for y in range(self.max_y):
+            self.map[y] = self.map[y][:self.max_x]
+
+    def do_folds(self, folds: List[str]) -> None:
+        for fold in folds:
+            self.do_fold(fold)
+            if self.fold1_dots == 0:
+                self.fold1_dots = self.count_dots()
+
+    def do_fold(self, fold: str) -> None:
+        dim, fold_at = fold.split('=')
+        fold_at = int(fold_at)
+        if dim == 'x':
+            self.fold_x(fold_at)
+        elif dim == 'y':
+            self.fold_y(fold_at)
+        self.trim_map()
+
+    def fold_x(self, fold_at: int) -> None:
+        lower = max(2 * fold_at - self.max_x + 1, 0)
+        for x in range(lower, fold_at):
+            for y in range(self.max_y):
+                self.map[y][x] = min(self.map[y][x] + self.map[y][2 * fold_at - x], 1)
+        self.max_x = fold_at
+
+    def fold_y(self, fold_at: int) -> None:
+        lower = max(2 * fold_at - self.max_y + 1, 0)
+        for x in range(self.max_x):
+            for y in range(lower, fold_at):
+                self.map[y][x] = min(self.map[y][x] + self.map[2 * fold_at - y][x], 1)
+        self.max_y = fold_at
+
+    def count_dots(self) -> int:
+        sum_dots = 0
+        for x in range(self.max_x):
+            for y in range(self.max_y):
+                sum_dots += self.map[y][x]
+        return sum_dots
+
+    def print_out(self) -> None:
         for y in range(self.max_y):
             line_out = ''
             for x in range(self.max_x):
