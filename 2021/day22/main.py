f0a60fea70371cba3829f1f7658387d898b78ddd
James MacAdie <james@macadie.co.uk>
2021-12-23 01:09:35 +0000

    Day 22 #3
    Fairly major re-write to just track all the intersections. Looks a
    lot quicker and still passes part 1. About to try on part 2!

--- 2021/day22/main.py
+++ 2021/day22/main.py
@@ -1,139 +1,119 @@
-from typing import NamedTuple, List, Tuple
-
-class Coord(NamedTuple):
-    x: int
-    y: int
-    z: int
-
-    def copy(self) -> 'Coord':
-        return Coord(self.x, self.y, self.z)
-
-    def __eq__(self, __o: object) -> bool:
-        if not isinstance(__o, Coord):
-            return NotImplemented
-        return self.x == __o.x and self.y == __o.y and self.z == __o.z
-
-class PointVal(NamedTuple):
-    loc: Coord
-    val: bool
+from typing import List, Tuple, Optional
 
 class Cube:
 
     def __init__(
         self,
-        x_start: int, x_end: int,
-        y_start: int, y_end: int,
-        z_start: int, z_end: int,
-        val: bool
+        x_range: Tuple[int, int],
+        y_range: Tuple[int, int],
+        z_range: Tuple[int, int]
         ) -> None:
-        self.x_start = x_start
-        self.y_start = y_start
-        self.z_start = z_start
-        self.x = list(range(x_start, x_end + 1))
-        self.y = list(range(y_start, y_end + 1))
-        self.z = list(range(z_start, z_end + 1))
-        self.vals = [] # type: List[List[List[int]]]
-        for _ in self.z:
-            plane = [] # type: List[List[int]]
-            for _ in self.y:
-                line = [] # type: List[int]
-                for _ in self.x:
-                    line.append(val)
-                plane.append(line)
-            self.vals.append(plane)
+        self.x_range = x_range
+        self.y_range = y_range
+        self.z_range = z_range
+        self.size = self.get_size()
 
-    def get_val_at_point(self, loc: Coord) -> int:
-        x = loc.x - self.x_start
-        y = loc.y - self.y_start
-        z = loc.z - self.z_start
-        return self.vals[z][y][x]
+    def intersection(self, other: 'Cube') -> Optional['Cube']:
+        if self.out_of_range(self.x_range, other.x_range):
+            return None
+        if self.out_of_range(self.y_range, other.y_range):
+            return None
+        if self.out_of_range(self.z_range, other.z_range):
+            return None
+        x_range = (max(self.x_range[0], other.x_range[0]),
+                min(self.x_range[1], other.x_range[1]))
+        y_range = (max(self.y_range[0], other.y_range[0]),
+                min(self.y_range[1], other.y_range[1]))
+        z_range = (max(self.z_range[0], other.z_range[0]),
+                min(self.z_range[1], other.z_range[1]))
+        return Cube(x_range, y_range, z_range)
 
-    def get_all_points(self) -> List[PointVal]:
-        points = []
-        for x in self.x:
-            for y in self.y:
-                for z in self.z:
-                    point = Coord(x, y, z)
-                    val = self.get_val_at_point(point)
-                    points.append(PointVal(point, val))
-        return points
+    @staticmethod
+    def out_of_range(range1: Tuple[int, int], range2:Tuple[int,int]) -> bool:
+        if range1[1] < range2[0]:
+            return True
+        if range1[0] > range2[1]:
+            return True
+        return False
 
-    def update(self, update: PointVal) -> None:
-        if update.loc.x in self.x and update.loc.y in self.y and update.loc.z in self.z:
-            self.vals[update.loc.z][update.loc.y][update.loc.x] = update.val
-
-class CubeFactory:
-
-    RANGE = 51
-
-    def __init__(self, line: str) -> None:
-        val_str, coords = line.split(' ')
-        self.val = (val_str == 'on')
-        x_coords, y_coords, z_coords = coords.split(',')
-        self.x_start, self.x_end = self.process_coords(x_coords)
-        self.y_start, self.y_end = self.process_coords(y_coords)
-        self.z_start, self.z_end = self.process_coords(z_coords)
-
-    def process_coords(self, coords_str: str) -> Tuple[int, int]:
-        temp = coords_str.split('=')[1]
-        start, end = temp.split('..')
-        start_i = min(max(int(start), -self.RANGE), self.RANGE)
-        end_i = min(max(int(end), -self.RANGE), self.RANGE)
-        return start_i, end_i
-
-    def get_cube(self) -> Cube:
-        return Cube(
-            self.x_start, self.x_end,
-            self.y_start, self.y_end,
-            self.z_start, self.z_end,
-            self.val)
+    def get_size(self) -> int:
+        x_dim = self.x_range[1] - self.x_range[0] + 1
+        y_dim = self.y_range[1] - self.y_range[0] + 1
+        z_dim = self.z_range[1] - self.z_range[0] + 1
+        return x_dim * y_dim * z_dim
 
 class Reactor:
 
-    RANGE = 50
-
-    def __init__(self) -> None:
-        self.map = Cube(
-            -self.RANGE, self.RANGE,
-            -self.RANGE, self.RANGE,
-            -self.RANGE, self.RANGE,
-            False)
-
-    def reboot_step(self, cube: Cube) -> None:
-        for point in cube.get_all_points():
-            self.map.update(point)
-
-    def get_on_count(self) -> int:
-        count = 0
-        for point in self.map.get_all_points():
-            if point.val:
-                count += 1
-        return count
-
-class RebootFactory:
-
-    def __init__(self, file_path: str) -> None:
-        self.reactor = Reactor()
+    def __init__(self, file_path: str, range_limit: int=0) -> None:
+        self.limit = range_limit
+        self.add_cubes = [] # type: List[Cube]
+        self.sub_cubes = [] # type: List[Cube]
         self.process_file(file_path)
 
     def process_file(self, file_path: str) -> None:
         with open(file_path, encoding='UTF-8') as file:
             while line := file.readline():
                 line = line.replace('\n', '').strip()
-                update_cube = CubeFactory(line).get_cube()
-                self.reactor.reboot_step(update_cube)
+                val_str, coords = line.split(' ')
+                val = (val_str == 'on')
+                new = self.get_cube(coords)
+                if new is not None:
+                    self.process_cube(new, val)
 
-#R = Reactor()
-#C = CubeFactory('on x=10..12,y=10..12,z=10..12').get_cube()
-#R.reboot_step(C)
-#print(R.get_on_count())
+    def process_cube(self, cube: Cube, add: bool) -> None:
+        new_subs = self.get_intersections(cube, self.add_cubes)
+        new_adds = self.get_intersections(cube, self.sub_cubes)
+        self.add_cubes += new_adds
+        self.sub_cubes += new_subs
+        if add:
+            self.add_cubes += [cube]
 
-RF = RebootFactory('test1.txt')
-assert RF.reactor.get_on_count() == 39
+    @staticmethod
+    def get_intersections(cube: Cube, previous: List[Cube]) -> List[Cube]:
+        output = [] # type: List[Cube]
+        for prev_cube in previous:
+            intersection = prev_cube.intersection(cube)
+            if intersection is not None:
+                output.append(intersection)
+        return output
 
-RF = RebootFactory('test2.txt')
-assert RF.reactor.get_on_count() == 590784
+    def get_cube(self, coords: str) -> Optional[Cube]:
+        x_coords, y_coords, z_coords = coords.split(',')
+        x_range = self.process_coords(x_coords)
+        if x_range is None:
+            return None
+        y_range = self.process_coords(y_coords)
+        if y_range is None:
+            return None
+        z_range = self.process_coords(z_coords)
+        if z_range is None:
+            return None
+        return Cube(x_range, y_range, z_range)
 
-RF = RebootFactory('input.txt')
-assert RF.reactor.get_on_count() == 607573
-print(RF.reactor.get_on_count())
+    def process_coords(self, coords_str: str) -> Optional[Tuple[int, int]]:
+        temp = coords_str.split('=')[1]
+        start, end = [int(i) for i in temp.split('..')]
+        if self.limit > 0:
+            if end < -self.limit or start > self.limit:
+                return None
+            start = max(start, -self.limit)
+            end = min(end, self.limit)
+        return start, end
+
+    def get_on_count(self) -> int:
+        output = 0
+        for cube in self.add_cubes:
+            output += cube.size
+        for cube in self.sub_cubes:
+            output -= cube.size
+        return output
+
+R = Reactor('test1.txt')
+assert R.get_on_count() == 39
+
+R = Reactor('test2.txt', 50)
+assert R.get_on_count() == 590784
+
+R = Reactor('input.txt', 50)
+assert R.get_on_count() == 607573
+print(R.get_on_count())
